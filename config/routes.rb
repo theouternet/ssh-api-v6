115483a6f00ba2dea288dc5f6c162c3ee9659444
theouternet <jrougeau@gmail.com>
2020-08-01 03:11:50 -0500

    db config success + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+      resources :categories, only: [:index, :show]
+      resources :softwares, only: [:index, :show]
+
+
+    end
+  end
 end
