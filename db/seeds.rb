2e6ff418edaeedc738317d53e92ab8b01919f71d
theouternet <jrougeau@gmail.com>
2020-08-01 03:22:09 -0500

    pg inst: github.com/learn-co-curriculum/wsl-setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+categories = Category.create([{ category_name: 'Accounting' }, { category_name: 'Music' }, { category_name: 'Design' }, { category_name: 'Statistics' }, { category_name: 'Spreadsheets' }])
+
+Software.create(software_name: 'Quicken', description: 'Do taxes.', category: categories.first)
+Software.create(software_name: 'Cakewalk', description: 'Make music.', category: categories.second)
+Software.create(software_name: 'InDesign', description: 'Design shit.', category: categories.third)
+Software.create(software_name: 'SPSS', description: 'Do stats.', category: categories.fourth)
+Software.create(software_name: 'Excel', description: 'Make tables.', category: categories.fifth)
+
+
+
+
